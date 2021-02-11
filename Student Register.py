699b1faa72368cbde5cfe16a331d625a6f514d6f
Joseph Sherman <shermanator2018@gmail.com>
2021-02-11 13:09:58 -0600

    Finished Student OOP

--- Student Register.py
+++ Student Register.py
@@ -1,1 +1,17 @@
 import StudentClass as s
+
+
+def main():
+    stu_id = input("Please enter your student ID: ")
+    name = input("Please enter your name: ")
+    dob = input("Please enter your date of birth (month, day, year): ")
+    cl = input("Please enter your classification (F, S, Jr, or Sr): ")
+
+    my_student = s.Student(stu_id, name, dob, cl)
+
+    print("Your age is " + str(my_student.calculateAge()) + ".")
+
+    my_student.registerRange()
+
+
+main()
--- StudentClass.py
+++ StudentClass.py
@@ -9,15 +9,38 @@ class Student:
         self.__classification = c
 
     def calculateAge(self):
+        # First split age into string then re arrange
+        birthDate = self.__dateOfBirth.split()
+        birthDay = birthDate[1]
+        birthMonth = birthDate[0]
+        birthYear = birthDate[2]
+
+        # Strip the comma from the entry
+
+        birthDay = birthDay.rstrip(",")
+        birthMonth = birthMonth.rstrip(",")
+        birthYear = birthYear.rstrip(",")
+
+        # Convert strings to int
+        birthDay = int(birthDay)
+        birthMonth = int(birthMonth)
+        birthYear = int(birthYear)
+
+        # Calculate age based on current day
         today = date.today()
-        age = today - self.__dateOfBirth
+        age = (
+            today.year - birthYear - ((today.month, today.day) < (birthMonth, birthDay))
+        )
         return age
 
     def registerRange(self):
-        if self.__classification == "freshman":
-            #display range
-        else if:
-
-        else if:
-
+        if self.__classification == "F":
+            print("Your registration is open 11/10 through 11/12.")
+        elif self.__classification == "S":
+            print("Your registration is open 11/7 through 11/9.")
+        elif self.__classification == "Jr":
+            print("Your registration is open 11/4 through 11/6.")
+        elif self.__classification == "Sr":
+            print("Your registration is open 11/1 through 11/3.")
         else:
+            print("Classification not specified.")
