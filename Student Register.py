b8755d4150d492e1f38c046c09058319684d59a8
Joseph Sherman <shermanator2018@gmail.com>
2021-02-15 11:29:23 -0600

    Student edit

--- Student Register.py
+++ Student Register.py
@@ -4,7 +4,7 @@ import StudentClass as s
 def main():
     stu_id = input("Please enter your student ID: ")
     name = input("Please enter your name: ")
-    dob = input("Please enter your date of birth (month, day, year): ")
+    dob = input("Please enter your numeric date of birth (month, day, year): ")
     cl = input("Please enter your classification (F, S, Jr, or Sr): ")
 
     my_student = s.Student(stu_id, name, dob, cl)
